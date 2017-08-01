78bbbdfc9834d23bb223acd8c9fc9a98399c3238
danielElopez <dlopez@osisoft.com>
2017-08-01 17:31:37 -0500

    Added a fast install script

--- install_runFromAnAdminCommandPrompt.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo ---------------------------------------------
-echo Running copy...
-robocopy "IOT Demo\IOT Demo" C:\inetpub\wwwroot *.* /mir /R:5 > installLog.txt
-echo ---------------------------------------------
-echo Check installLog.txt for details. Press ENTER to quit.
-set /p x= 
